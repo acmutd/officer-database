bdf55e4bee333781b51d40afe4e5c7d127c209e6
Ruben Olano <rubendavid2711@gmail.com>
2025-11-22 23:03:29 -0600

    change quick links to reminders for future use

--- src/routes/_authed/index.tsx
+++ src/routes/_authed/index.tsx
@@ -52,7 +52,7 @@ function App() {
 			<div className="flex w-1/2 flex-col rounded-xl border border-white/10 bg-gradient-to-br from-white/5 to-white/10 px-8 shadow-xl backdrop-blur-xl">
 				<div className="mb-4 w-1/3 self-center rounded-b-xl bg-white/5 p-4 pt-0">
 					<h2 className="my-2 text-center text-2xl font-bold text-white">
-						my quick links
+						reminders
 					</h2>
 				</div>
 			</div>
