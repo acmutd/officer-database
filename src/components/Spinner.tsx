5a16e5e82e163fff6745c40fe61d9667dada1a0c
Ruben Olano <rubendavid2711@gmail.com>
2025-12-02 21:10:50 -0600

    Make spinner not foce scroll

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,6 +1,6 @@
 export function Spinner() {
 	return (
-		<div className="flex h-screen w-full items-center justify-center">
+		<div className="flex h-full w-full items-center justify-center">
 			<div className="size-10 rounded-full border-4 border-gray-200 border-t-foreground animate-spin" />
 		</div>
 	);
